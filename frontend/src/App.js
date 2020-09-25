e6645df6430dff69456795eb0705799ecc805f7b
bhaktibj <bhaktibj402@gmail.com>
2020-09-25 17:26:57 +0530

    added chnages in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
-import './App.css';
-import ProductComponent from './components/ProductComponent';
-import HomeComponent from './components/HomeComponent';
-import CartComponent from './components/CartComponent';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
+// import React from 'react';
+// import { BrowserRouter, Route, Link } from 'react-router-dom'
+// import './App.css';
+// import ProductComponent from './components/ShowProductComponent';
+// import HomeComponent from './components/ProductsComponent';
+// import CartComponent from './components/CartComponent';
+// import Header from './components/HeaderComponent';
+// import Home from './components/HomeComponent';
+// import LoginComponent from './components/LoginComponent';
 
-function App() {
-  return (
-    <>
-    <Header/>
-    <div className="top-element-formatting">
-    <h1>{" "}</h1>
-    <BrowserRouter>
-            <Route path="/product/:id" component={ProductComponent} />
-            <Route path="/cart/:id?" component={CartComponent} />
-            <Route path="/products" component={HomeComponent} />
-            <Route exact path="/" component={Home} />
-    </BrowserRouter>
-    </div>
-    {/* <Footer/> */}
-    </>
-  );
-  }
-export default App;
+// function App() {
+//   return (
+//     <>
+//     <Header/>
+//     <div className="top-element-formatting">
+//     <h1>{" "}</h1>
+//     <BrowserRouter>
+//             <Route path="/product/:id" component={ProductComponent} />
+//             <Route path="/cart/:id?" component={CartComponent} />
+//             <Route path="/products" component={HomeComponent} />
+//             <Route path="/login" component={Login}/>
+//             <Route exact path="/" component={Home} />
+//     </BrowserRouter>
+//     </div>
+//     {/* <Footer/> */}
+//     </>
+//   );
+//   }
+// export default App;
